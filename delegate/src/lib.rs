395e44ffe57c0bf3be58add4526450b1f4190736
wistchze <handidwic1225@gmail.com>
2025-03-12 11:48:03 +0700

    Adds broadcast macro

--- delegate/src/lib.rs
+++ delegate/src/lib.rs
@@ -130,3 +130,17 @@ macro_rules! async_listens {
         });
     }};
 }
+
+#[macro_export]
+macro_rules! broadcast {
+    ($delegate_name:literal, $data:expr) => {
+        self.get_delegate_manager().broadcast($delegate_name, $data);
+    };
+}
+
+#[macro_export]
+macro_rules! async_broadcast {
+    ($delegate_name:literal, $data:expr) => {
+        self.get_delegate_manager().async_broadcast($delegate_name, $data);
+    };
+}
